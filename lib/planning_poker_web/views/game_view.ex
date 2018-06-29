2b64330c0f7c23ef1f51614bf4aac9d32b452806
Chris Westra <cwestra@gmail.com>
2018-06-29 10:51:33 -0400

    WIP styling game page

--- lib/planning_poker_web/views/game_view.ex
+++ lib/planning_poker_web/views/game_view.ex
@@ -1,3 +1,18 @@
 defmodule PlanningPokerWeb.GameView do
   use PlanningPokerWeb, :view
+  alias PlanningPoker.Rounds.Estimate
+
+  def vote_string(%{status: status}, %Estimate{} = estimate) do
+    email = estimate.user.email
+    pending = Estimate.pending?(estimate)
+
+    vote = case {status, pending} do
+      {"closed", true} -> "did not vote"
+      {"closed", false} -> "voted #{estimate.amount}"
+      {_ , true} -> "has not voted"
+      {_ , false} -> "has voted"
+    end
+
+    [email, " ", vote]
+  end
 end
